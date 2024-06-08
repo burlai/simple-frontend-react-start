1bce7c846b68852dce2afe8031682a9919daa664
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2024-06-08 10:02:49 +0200

    simple array log

--- src/components/PricesList.tsx
+++ src/components/PricesList.tsx
@@ -2,6 +2,12 @@ const prices = [
   3.902341879, 7.123456, 5.6789, 9.05244656, 18.6802874, 325.76830948,
 ];
 
+console.log("старий масив: ", prices);
+console.log(
+  "новий масив: ",
+  prices.map((price, index) => `${price.toFixed(2)}, index: ${index}`)
+);
+
 const PriceList = () => {
   return (
     <div>
