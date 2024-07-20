d50802b9b0043acc201a94acd86613341c49f7bf
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2024-07-20 11:46:46 +0200

    default app style changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="page-header">Page header</h1>
+      <h1 className="page-header">React українською</h1>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+        alt="React Logo"
+        className="react-logo"
+      />
+
+      <div className="component-container"></div>
     </>
   );
 }
