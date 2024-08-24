6d85e8ca0a4c287ef1de4e548ddd2dad7cff943a
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2024-08-24 13:22:25 +0200

    chore: Add useImperativeHandle hook to CustomInput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { useRef } from "react";
 import "./App.css";
+import { CustomInput, CustomInputHandle } from "./components/CustomInput";
 
 function App() {
+  const inputRef = useRef<CustomInputHandle>(null);
+
+  const handleFocus = () => {
+    inputRef.current?.focus();
+  };
+
+  const handleClear = () => {
+    inputRef.current?.clear();
+  };
+
+  // const handleValue = () => {
+  //   inputRef.current?.setValue();
+  // };
+
   return (
     <>
       <h1 className="page-header">React українською</h1>
@@ -10,7 +26,20 @@ function App() {
         className="react-logo"
       />
 
-      <div className="component-container"></div>
+      <div className="component-container">
+        <h2>useImperativeHandle hook</h2>
+        <CustomInput ref={inputRef} />
+        <button className="button" onClick={handleFocus}>
+          Focus input
+        </button>
+        <button className="button" onClick={handleClear}>
+          Clear input
+        </button>
+
+        {/* <button className="button" onClick={handleValue}>
+          Set value
+        </button> */}
+      </div>
     </>
   );
 }
---
+++ src/components/CustomInput.tsx
@@ -0,0 +1,35 @@
+import { useImperativeHandle, forwardRef, useRef } from "react";
+
+export type CustomInputHandle = {
+  focus: () => void;
+  clear: () => void;
+  //   setValue: () => void;
+};
+
+export const CustomInput = forwardRef<CustomInputHandle>((_, ref) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useImperativeHandle(
+    ref,
+    () => ({
+      focus: () => {
+        if (inputRef.current) {
+          inputRef.current.focus();
+        }
+      },
+      clear: () => {
+        if (inputRef.current) {
+          inputRef.current.value = "";
+        }
+      },
+      //   setValue: () => {
+      //     if (inputRef.current) {
+      //       inputRef.current.value = "some text";
+      //     }
+      //   },
+    }),
+    []
+  );
+
+  return <input ref={inputRef} type="text" />;
+});
